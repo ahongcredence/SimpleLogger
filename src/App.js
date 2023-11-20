0066a70eeee36d2dd8e75725c8d64f0e8ed13adb
ahong1234 <hexyield@gmail.com>
2023-11-20 16:28:32 -0500

    got basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function LogInput() {
+  const [log, setLog] = useState({
+    systemCode: '',
+    logLevel: 'debug', // Default to 'debug'
+    message: ''
+  });
+
+  const handleInputChange = (e) => {
+    setLog({ ...log, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const timestamp = new Date().toISOString();
+    try {
+      await axios.post('https://ic2sabz0ck.execute-api.us-east-2.amazonaws.com/default/storeMyLogs1', { ...log, timestamp });
+      alert('Log submitted successfully');
+      setLog({ systemCode: '', logLevel: 'debug', message: '' }); // Reset log level to 'debug'
+    } catch (error) {
+      console.error('Error submitting log');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      maxWidth: '300px',
+      margin: 'auto',
+      height: '100vh',
+      justifyContent: 'center',
+      backgroundColor: '#333', // Dark background color
+      color: '#fff', // Light text color
+      padding: '20px',
+      borderRadius: '10px', // Rounded corners
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)' // Box shadow for depth
+    }}>
+      <label htmlFor="systemCode">System Code:</label>
+      <input type="text" id="systemCode" name="systemCode" placeholder="Enter System Code" value={log.systemCode} onChange={handleInputChange} />
+
+      <label htmlFor="logLevel">Log Level:</label>
+      <select id="logLevel" name="logLevel" value={log.logLevel} onChange={handleInputChange}>
+        <option value="debug">Debug</option>
+        <option value="trace">Trace</option>
+        <option value="error">Error</option>
+        <option value="info">Info</option>
+      </select>
+
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" placeholder="Enter Message" value={log.message} onChange={handleInputChange} />
+
+      <button type="submit" style={{ backgroundColor: '#4CAF50', color: '#fff', cursor: 'pointer' }}>Submit Log</button>
+    </form>
   );
 }
 
-export default App;
+export default LogInput;
